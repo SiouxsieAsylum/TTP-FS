764dc87ab6287e3f30c3441b07934d41b6adcaa7
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2019-06-29 12:09:35 -0400

    fixed formatting of inital migration

--- db/migrations/mig_062920191111.sql
+++ db/migrations/mig_062920191111.sql
@@ -8,18 +8,20 @@ CREATE TABLE Users (
     email VARCHAR NOT NULL,
     password VARCHAR, 
     balance INT NOT NULL
-)
+);
 
 CREATE TABLE Portfolios (
     portfolioId SERIAL PRIMARY KEY,
-    userId INT FOREIGN KEY (userId) REFERENCES Users(userId),
+    userId INT, 
+    FOREIGN KEY (userId) REFERENCES Users(userId),
     name VARCHAR
-)
+);
 
 CREATE TABLE Stocks (
     stockId SERIAL PRIMARY KEY,
     tickerSymbol VARCHAR NOT NULL,
-    portfolioId INT FOREIGN KEY (portfolioId) REFERENCES Portfolios(portfolioId)
+    portfolioId INT, 
+    FOREIGN KEY (portfolioId) REFERENCES Portfolios(portfolioId),
     buyingPrice INT NOT NULL,
     datePurchased DATE NOT NULL DEFAULT CURRENT_DATE
-)
+);
